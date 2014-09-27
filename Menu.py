82c4829a34a63aed1cd8c78f26ae16ce6ff92e76
jaym910 <jayluptonmiller@gmail.com>
2014-09-27 18:24:29 -0400

    updated menu

--- Menu.py
+++ Menu.py
@@ -6,6 +6,9 @@ import State
 
 class Menu(State.State):
     FONT = None
+    INIT_X = 10
+    INIT_Y = 300
+    Y_SPACING = 30
     def __init__(self):
         State.State.__init__(self)
         if not Menu.FONT:
@@ -13,23 +16,36 @@ class Menu(State.State):
 
         self.menu_strings = ["New Game", "Adjust Brightness", "Adjust Sound", \
             "Display Highscores", "Quit"]
+        self.surfs = []
+        for i in range(5):
+            self.surfs.append(Menu.FONT.render(self.menu_strings[i], True, (255, 255, 255)))
+        self.selected = []
+        for i in range(5):
+            self.selected.append(False)
 
     def render(self):
         G.Globals.SCREEN.fill((0,0,0))
         
-        x_cord = 10
-        y_cord = 300
-        y_spacing = 20
+        x_cord = Menu.INIT_X
+        y_cord = Menu.INIT_Y
+        y_spacing = Menu.Y_SPACING        
 
-        for i in range(5):
-            surf = Menu.FONT.render(self.menu_strings[i], True, (255, 255, 255))
-            if self.check_mouse(x_cord, y_cord, surf.get_width(), surf.get_height()):
-                surf = Menu.FONT.render(self.menu_strings[i], True, (255, 0, 0))
+        for surf in self.surfs:
             G.Globals.SCREEN.blit(surf, (x_cord, y_cord))
             y_cord += (surf.get_height() + y_spacing)
 
     def update(self, time):
-        pass
+        x_cord = Menu.INIT_X
+        y_cord = Menu.INIT_Y
+        y_spacing = Menu.Y_SPACING
+        for i in range(5):
+            self.surfs[i] = Menu.FONT.render(self.menu_strings[i], True, (255, 255, 255))
+            surf = self.surfs[i]
+            self.selected[i] = False
+            if self.check_mouse(x_cord, y_cord, surf.get_width(), surf.get_height()):
+                self.surfs[i] = Menu.FONT.render(self.menu_strings[i], True, (255, 0, 0))
+                self.selected[i] = True
+            y_cord += (surf.get_height() + y_spacing)
 
     def event(self, event):
         pass
