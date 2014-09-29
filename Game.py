2ae81ec62aa23a1bc2c3921137bb987bfa43cdb7
jaym910 <jayluptonmiller@gmail.com>
2014-09-28 20:38:13 -0400

    added player animations

--- Game.py
+++ Game.py
@@ -17,6 +17,7 @@ class Game(State.State):
         self.all_sprites_list = PS.Group()
         self.enemy_speed = 1
         self.time = 0.0
+        self.enemies = []
         #PX.music.load("Some_Song.mod") #if you want to add music Jay 
         #PX.music.play(-1)
 
@@ -28,17 +29,22 @@ class Game(State.State):
                     new_y_vel = -self.enemy_speed
                 else:
                     new_y_vel = 0
-                self.all_sprites_list.add(Enemy.Enemy(new_x, new_y, new_x_vel, new_y_vel))  
-
+                new_enemy = Enemy.Enemy(new_x, new_y, new_x_vel, new_y_vel)
+                self.all_sprites_list.add(new_enemy)  
+                self.enemies.append(new_enemy)
         self.player = Player.Player(400, 300)
         self.all_sprites_list.add(self.player)
+
     def render(self):
         G.Globals.SCREEN.fill(PC.Color("black"))    
         self.all_sprites_list.draw(G.Globals.SCREEN)
+
     def update(self, time):
         self.time += time
         while self.time > G.Globals.INTERVAL:
-            self.all_sprites_list.update()
+            for e in self.enemies:
+                e.update()
+            self.player.update(G.Globals.Interval)
             self.time -= G.Globals.INTERVAL
     def event(self, event):
         if event.type == PG.KEYDOWN and event.key  == PG.K_ESCAPE:
