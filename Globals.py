de7315a2cada6103720c78678a34eb126e06bb65
TrevorAron <taron1@jhu.edu>
2014-11-16 20:32:28 -0500

    fuckin git

--- Globals.py
+++ Globals.py
@@ -13,5 +13,5 @@ class Globals(object):
 
 def new_level(player):
     Globals.STATE = Main.Game(2, 4, player)
-	if(Main.Game.LEVEL+1 <= Main.Game.MAX_LEVEL):
-		Globals.STATE = Main.Game(Main.Game.LEVEL+1, 4, player)
+    if(Main.Game.LEVEL+1 <= Main.Game.MAX_LEVEL):
+        Globals.STATE = Main.Game(Main.Game.LEVEL+1, 4, player)
